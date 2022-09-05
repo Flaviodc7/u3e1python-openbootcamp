5297cd8d656106d991e08b0c86758257fb76506c
Flavio Coscarella <flaviodc@live.com.ar>
2022-09-05 18:41:56 -0300

    Commit del ejercicio

---
+++ main.py
@@ -0,0 +1,15 @@
+nombre = "Jorge"
+apellidos = "Ortigoza Gutierrez"
+edad = 42
+email = "jorgeog@live.com"
+telefono = "4425-4568"
+casado = True
+conHijos = False
+listaDeAmigos = ["Ariel", "Eduardo", "Gonzalo"]
+peliculasVistas = {
+    "Terminator 2": "vista",
+    "Predator": "vista",
+    "Pocahontas": "no vista"
+}
+
+print(nombre, apellidos, edad, email, telefono, casado, conHijos, listaDeAmigos, peliculasVistas)
